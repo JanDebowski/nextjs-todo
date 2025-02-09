4b13260d3b2960d6bad624aebba1b79c925407d0
Jan Debowski <>
2025-02-09 19:31:14 +0100

    setup global-sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { GlobalSidebar } from "@/components/common/global-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <GlobalSidebar />
+          <SidebarTrigger />
+          <div className="w-screen h-screen">{children}</div>
+        </SidebarProvider>
       </body>
     </html>
   );
